6b7156fc7ca6e004491871330c48e3000e1a9d18
rokija <rokijagames@gmail.com>
2024-08-26 15:13:28 +0200

    init

---
+++ src/Navigation.jsx
@@ -0,0 +1,11 @@
+export function Navigation() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">All</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Work</a></li>
+            </ul>
+        </nav>
+    )
+}
