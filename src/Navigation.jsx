38606866034865baee802b2dd19e96c07c515400
rokija <rokijagames@gmail.com>
2024-08-26 18:34:49 +0200

    ui nav changes

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -2,9 +2,9 @@ export function Navigation() {
     return (
         <nav>
             <ul>
-                <li><a href="#">All</a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Work</a></li>
+                <li><a className="btn btn-nav" href="#">All</a></li>
+                <li><a className="btn btn-nav" href="#">About</a></li>
+                <li><a className="btn btn-nav" href="#">Work</a></li>
             </ul>
         </nav>
     )
